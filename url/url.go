8eaa6f61b45a2cd839df41f6b27185d7fc6db6d6
Mystlicious <Daffaaudyapramana03@gmail.com>
2023-03-24 23:00:13 +0700

    update

--- url/url.go
+++ url/url.go
@@ -11,11 +11,11 @@ func Web(page *fiber.App) {
 	page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
 	page.Get("/", controller.Homepage)
-	page.Get("/user", controller.Getuser) //API from user whatsapp message from iteung gowa
-	// page.Get("/pendaftaran", controller.Getpendaftaran)         //API from user whatsapp message from iteung gowa
-	// page.Get("/pembayaran", controller.Getpembayaran)   //API from user whatsapp message from iteung gowa
-	// page.Get("/pengumuman", controller.Getpengumuman) //API from user whatsapp message from iteung gowa
-	// page.Get("/kursus", controller.Getkursus) //API from user whatsapp message from iteung gowa
+	page.Get("/user", controller.Getuser)               //API from user whatsapp message from iteung gowa
+	page.Get("/pendaftaran", controller.Getpendaftaran) //API from user whatsapp message from iteung gowa
+	page.Get("/pembayaran", controller.Getpembayaran)   //API from user whatsapp message from iteung gowa
+	page.Get("/pengumuman", controller.Getpengumuman)   //API from user whatsapp message from iteung gowa
+	page.Get("/kursus", controller.Getkursus)           //API from user whatsapp message from iteung gowa
 
 	page.Get("/", controller.Sink)
 	page.Post("/", controller.Sink)
